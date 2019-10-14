e8578e685cc9578ca3d698e21bf3fed52ae48c3f
Excalibur0w0 <752408303@qq.com>
2019-10-14 15:44:29 +0800

    u

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default-dark.css' // This line here
+import 'material-design-icons/iconfont/material-icons.css'
+import router from './router/router'
+
+Vue.use(VueMaterial)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
+    router: router
 }).$mount('#app')
